faddd950a90ab13d081a2aa0ad4bf36ff3c5879e
Eduardo Dantas <dantasleld@gmail.com>
2018-08-07 00:51:25 -0300

    update files

--- JavaScript e jQuery/animais-e-florestas/js/app.js
+++ JavaScript e jQuery/animais-e-florestas/js/app.js
@@ -1,27 +1,22 @@
-$(document).ready(function () {
-  var classActive = 'active';
+$('[data-group]').each(function () {
+  const $allTarget = $(this).find('[data-target]');
+  const $allClick = $(this).find('[data-click]');
+  const activeClass = 'active';
 
-  $('.animais .tab-menu a').first().addClass(classActive);
-  $('.animais .item').first().addClass(classActive);
+  $allTarget.first().addClass(activeClass);
+  $allClick.first().addClass(activeClass);
 
-  $('.animais .tab-menu a').click(function (e) {
+  $allClick.click(function(e){
     e.preventDefault();
-    let itemID = $(this).attr('href');
+    
+    const id = $(this).data('click');
+    const $target = $('[data-target="' + id + '"]');
 
-    $('.animais .tab-menu a, .animais .item').removeClass(classActive);
-    $(this).addClass(classActive);
-    $(itemID).addClass(classActive);
+    $allClick.removeClass(activeClass);
+    $allTarget.removeClass(activeClass);
+
+    $(this).addClass(activeClass);
+    $target.addClass(activeClass);
   });
 
-  $('.florestas .tab-menu a').first().addClass(classActive);
-  $('.florestas .item').first().addClass(classActive);
-
-  $('.florestas .tab-menu a').click(function (e) {
-    e.preventDefault();
-    let itemID = $(this).attr('href');
-
-    $('.florestas .tab-menu a, .florestas .item').removeClass(classActive);
-    $(this).addClass(classActive);
-    $(itemID).addClass(classActive);
-  });
 });
---
+++ JavaScript e jQuery/nature/js/app.js
@@ -0,0 +1,21 @@
+$('[data-group]').each(function(){
+	var $allTarget = $(this).find('[data-target]'),
+			$allClick = $(this).find('[data-click]'),
+			activeClass = 'active';
+	
+	$allTarget.first().addClass(activeClass);
+	$allClick.first().addClass(activeClass);
+	
+	$allClick.click(function(e){
+		e.preventDefault();
+		
+		var id = $(this).data('click'),
+				$target = $('[data-target="' + id + '"]');
+		
+		$allClick.removeClass(activeClass);
+		$allTarget.removeClass(activeClass);
+		
+		$target.addClass(activeClass);
+		$(this).addClass(activeClass);
+	});
+});
